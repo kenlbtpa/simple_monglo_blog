f738e01cd1f9d23c0a9dc04b10f141d31119c195
Kenneth L <kl1248@nyu.edu>
2015-02-13 02:45:51 -0500

    minor fix

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,11 +1,22 @@
-function login(caller){ /*Expects either this or document.getElementbyId form */
+function buildFormData(caller, params){
 	var form = $(caller).is("form") ? $(caller) : $(this).closest("form");
-	var params = [ "email" , "password" ]; 
 	var data = params.reduce(function(prev, curr, i , arr){ 
 		var v = form.find( "[name="+ curr +"]" );  
 		console.log( "v" , v); 
 		if(v.length === 0) return prev;
 		prev[ arr[i] ] = v.val(); return prev;  
 	} , {} );
-	console.log(data);  
+	return data; 
+}
+
+function login(caller){ /*Expects either this or document.getElementbyId form */
+	var params = [ "email" , "password" ]; 
+	var data = buildFormData(caller, params); 
+	console.log(data); 
+}
+
+function register(caller){
+	var params = [ "nickname" , "email" , "password" , "password_confirmation" ]; 
+	var data = buildFormData(caller, params); 	
+	console.log(data); 
 }
