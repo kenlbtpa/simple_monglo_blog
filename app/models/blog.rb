2970fe3f9bf6629b39ed18ae7a240ce44a087c88
Kenneth L <kl1248@nyu.edu>
2015-02-12 23:36:31 -0500

    changes

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,31 @@
 class Blog
   include MongoMapper::Document
 
-  key :_id, Integer
-  key :blogger, Int
-  key :title, String
-  key :content, String
-  key :created_at, Time
-  key :updated_at, Time
+  before_validation :processBlog
+
+  key :_id, Integer , :required => true
+  key :blogger, Int , :required => true
+  key :title, String , :required => true
+  key :content, String , :required => true
+  key :created_at, Time , :required => true
+  key :updated_at, Time , :required => true
+
+  private
+
+  def processBlog
+  	
+	# Making Sure User exists
+	if User.find( blogger ) === nil
+		errors.add( :blogger , "Invalid Blogger" )
+	end
+  	
+  	if self.created_at === nil
+  		self.created_at = Time.now
+  	end
+	if self.updated_at === nil  	
+	  	self.updated_at = Time.now
+  	end
+  
+  end
 
 end
