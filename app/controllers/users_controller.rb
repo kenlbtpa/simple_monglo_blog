83176fa6edab69476dc6520d248f51e3b9ce924f
Kenneth L <kl1248@nyu.edu>
2015-02-13 11:53:48 -0500

    register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,12 @@ class UsersController < ApplicationController
 
 	def create
 		#Register
-		render json: params
+		if params[:password] != params[:password_confirmation]
+			render json: [ status: false , error: "Passwords are not the same." ] and return
+		end
+		@user = User.new( nickname: params[:nickname] , email: params[:email] , pass: params[:password] )
+		@user.save
+		render json: params and return
 	end
 
 
