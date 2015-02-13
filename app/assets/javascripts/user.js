2a39a67950ba151b7b017cffc5b0554f834c0b78
Kenneth L <kl1248@nyu.edu>
2015-02-13 02:24:56 -0500

    minor fix

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,6 @@
 function login(caller){
 	var form = $(this).closest("form"); 
 	var params = [ "email" , "password" ]; 
-	var data = params.reduce(function(prev, curr, i , arr){ curr["email"] = form.find( "[name~="+ arr[i] +"]" );  } , {} );
+	var data = params.reduce(function(prev, curr, i , arr){ curr["email"] = form.find( "[name~="+ arr[i] +"]" ); return curr;  } , {} );
 	console.log(data);  
 }
