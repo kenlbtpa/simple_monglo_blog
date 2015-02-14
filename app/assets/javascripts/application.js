e99651906016b76b301c42026b22d73d26e5d766
Kenneth L <kl1248@nyu.edu>
2015-02-13 22:48:46 -0500

    script update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require turbolinks
 //= require_tree .
 
