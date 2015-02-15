9778f9d2394d10f73ef1bc436633aa7344a6a5e5
Kenneth L <kl1248@nyu.edu>
2015-02-14 23:53:20 -0500

    blog created added

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post '/user/new' => 'users#new'
   post '/user/create' => 'users#create'
 
+  post '/blog/create' => 'blogs#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
