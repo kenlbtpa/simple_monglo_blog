577792c0895524656e11249891c8a9c49882d2e1
Kenneth L <kl1248@nyu.edu>
2015-02-15 00:57:55 -0500

    front end update

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -1,11 +1,12 @@
 function blog_create(caller){ /*Expects either this or document.getElementbyId form */
-	var params = [ "title" , "content" ]; 
+	var params = [ "title" , "content" , "image" ]; 
 	var data = buildFormData(caller, params); 
 	var form = $(caller).is("form") ? $(caller) : $(this).closest("form");
 	$.ajax({
 		url: form.attr("action"), 
 		type: form.attr("method"), 
 		data: data,
+		processData: false, 
 		error: function(jqxhr, response, status){ 
 			console.log(jqxhr, response, status); 
 			form.find( ".ajaxErrors" ).html( "<span style='color:red' > Your request could not be processed. </span>" ); 
